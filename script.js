1807ee909eaadfee8652ff2e839c81ed39faf349
budokaru777 <rf24059q@st.omu.ac.jp>
2026-07-08 21:28:27 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleHint(id) {
-  const element = document.getElementById(id);
-
-  if (element.style.display === "flex") {
-    element.style.display = "none";
-  } else {
-    element.style.display = "flex";
-  }
-}
